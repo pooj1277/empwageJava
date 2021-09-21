9884f851f493b070ea61a1c3b9a4ff746379f726
pooj1277 <pooja.sd3@gmail.com>
2021-09-21 10:53:53 -0700

    employee part time and wage computation

---
+++ EmpWageBuilderUC3.java
@@ -0,0 +1,26 @@
+//ADD PART TIME EMPLOYEE AND WAGE
+package com.empwage;
+
+public class EmpWageBuilderUC3{
+
+    public static void main(String[] args){
+            //constants
+			int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+			int EMP_RATE_PER_HOUR = 20;
+			//variables
+			int empWage = 0;
+			int empHrs = 0;
+            //computation
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            if ( empCheck == IS_PART_TIME)
+                    empHrs = 4;
+			else if ( empCheck == IS_FULL_TIME)
+                    empHrs = 8;
+            else
+                    empHrs = 0;
+			
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage: " +empWage);
+    }
+}
