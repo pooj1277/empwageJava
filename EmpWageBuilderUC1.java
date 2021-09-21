4ad21529c9051562e2071f15ff576f052d766c55
pooj1277 <pooja.sd3@gmail.com>
2021-09-21 10:50:47 -0700

    emplyee attendance

---
+++ EmpWageBuilderUC1.java
@@ -0,0 +1,19 @@
+/* CHECK EMPLOYEE PRESENT OR ABSENT
+*/
+package com.empwage;
+
+public class EmpWageBuilderUC1{
+
+    public static void main(String[] args){
+            //constants
+            int IS_FULL_TIME = 1;
+            //computation
+            double empCheck = Math.floor(Math.random() * 10) % 2;
+            if ( empCheck == IS_FULL_TIME)
+                    System.out.println("Employee is Present");
+            else
+                    System.out.println("Employee is Absent");
+            //return 0;
+    }
+    
+}
