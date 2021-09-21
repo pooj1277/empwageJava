b7cbfd0ee5f520d084a20e67f7b9bef1c9f2af37
pooj1277 <pooja.sd3@gmail.com>
2021-09-21 10:48:37 -0700

    Welcome to employee wage computation program

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.empwage;
+
+/**
+ * Welcome to Employee Wage Computation Program
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Wage Computation Program" );
+    }
+}
